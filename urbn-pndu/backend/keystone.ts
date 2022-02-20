a5dc5ca6b0c43e5dcd7a6ca50908628ea08016e7
ArtisticCollective <shahbaz@artisticcollective.ca>
2022-02-20 20:09:51 +0530

    Fix

--- urbn-pndu/backend/keystone.ts
+++ urbn-pndu/backend/keystone.ts
@@ -1,9 +1,12 @@
 import { config, createSchema } from '@keystone-next/keystone/schema';
 import 'dotenv/config';
-import { createAuth } from "@keystone-next/auth";
-import { withItemData, statelessSessions} from "@keystone-next/keystone/session";
-import { User } from "./schemas/User";
-import { Product } from "./schemas/Product";
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
 import { insertSeedData } from './seed-data';
 
@@ -16,46 +19,46 @@ const sessionConfig = {
 };
 
 const { withAuth } = createAuth({
-    listKey: 'User',
-    identityField: 'email',
-    secretField: 'password',
-    initFirstItem: {
-        fields: ['name', 'email', 'password'],
-        // TODO: Add in inital roles
-    }
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add in inital roles
+  },
 });
 
-export default withAuth(config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
     },
-  },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    // TODO: Add data seeding here
-    async onConnect(keystone) {
-      if (process.argv.includes('--seed-data')) {
-        await insertSeedData(keystone);
-      }
-    }
-  },
-  lists: createSchema({
-    // Schema items go in here
-    User,
-    Product,
-    ProductImage
-  }),
-  ui: {
-    // Show UI only for people who pass this test
-    isAccessAllowed: ({ session }) => {
-        return !!session?.data;
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: Add data seeding here
+      async onConnect(keystone) {
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
-  },
-  // TODO: Add session values here.
-  session: withItemData(statelessSessions(sessionConfig), {
-    User: `id name email`
-})
-}));
+    lists: createSchema({
+      // Schema items go in here
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      // Show UI only for people who pass this test
+      isAccessAllowed: ({ session }) => !!session?.data,
+    },
+    // TODO: Add session values here.
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id name email',
+    }),
+  })
+);
